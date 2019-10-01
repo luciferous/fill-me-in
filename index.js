adf459969437f112608a23ff0e9a6a2ff9b1ae9c
Neuman Vong <neuman.vong@gmail.com>
2019-09-30 23:45:46 -0400

    npm update / target es6

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 function newFunction(defn) {
-    var ix0 = defn.indexOf("function(");
+    let ix0 = defn.indexOf("function(");
     if (ix0 == -1) {
         return new Function(defn);
     }
     ix0 += "function(".length;
-    var ix1 = defn.indexOf(")", ix0);
+    let ix1 = defn.indexOf(")", ix0);
     if (ix1 == -1) {
-        throw new Error("expected ')' after index " + ix0 + ": " + defn);
+        throw new Error(`expected ')' after index ${ix0}: ${defn}`);
     }
     ix1 += 1;
-    var ix2 = defn.indexOf("{", ix1);
+    let ix2 = defn.indexOf("{", ix1);
     if (ix2 == -1) {
-        throw new Error("expected '{' after index " + ix1 + ": " + defn);
+        throw new Error(`expected '{' after index ${ix1}: ${defn}`);
     }
     ix2 += 1;
-    var ix3 = defn.lastIndexOf("}");
+    let ix3 = defn.lastIndexOf("}");
     if (ix3 == -1) {
-        throw new Error("expected '}': " + defn);
+        throw new Error(`expected '}': ${defn}`);
     }
-    var args = defn.substring(ix0, ix1 - 1);
-    var body = defn.substring(ix2, ix3);
+    let args = defn.substring(ix0, ix1 - 1);
+    let body = defn.substring(ix2, ix3);
     return new Function(args, body);
 }
 function textContent(e) {
@@ -28,8 +28,7 @@ function textContent(e) {
 function unpackObject(e) {
     if (typeof e.value !== "object")
         return false;
-    for (var _i = 0, _a = Object.keys(e.value); _i < _a.length; _i++) {
-        var attr = _a[_i];
+    for (let attr of Object.keys(e.value)) {
         if (attr == "textContent") {
             this.textContent = e.value[attr];
         }
@@ -43,12 +42,12 @@ function imageSource(e) {
         return false;
     this.setAttribute("src", e.value.toString());
 }
-export var Modifiers = {
+export const Modifiers = {
     textContent: textContent,
     unpackObject: unpackObject,
     imageSource: imageSource
 };
-var defaultModifiers = [
+const defaultModifiers = [
     unpackObject,
     imageSource,
     textContent
@@ -92,22 +91,22 @@ export function render(target, values, options) {
         modifiers: defaultModifiers
     }, options || {});
     if (typeof target === "string") {
-        var template = document.querySelector(target);
+        let template = document.querySelector(target);
         if (template instanceof HTMLTemplateElement) {
             return render(template, values, options);
         }
-        throw new Error("template not found: " + target);
+        throw new Error(`template not found: ${target}`);
     }
     else if (target instanceof HTMLTemplateElement) {
-        var fragment = render(document.importNode(target.content, true), values, options);
+        let fragment = render(document.importNode(target.content, true), values, options);
         if (target.parentElement && options.replace) {
             target.parentElement.insertBefore(fragment, target);
             target.remove();
         }
         return fragment;
     }
-    var refs = [];
-    for (var i = 0; i < target.children.length; i++) {
+    let refs = [];
+    for (let i = 0; i < target.children.length; i++) {
         refs.push([target.children[i], values]);
     }
     go(refs, options.modifiers);
@@ -115,8 +114,8 @@ export function render(target, values, options) {
 }
 function go(refs, modifiers) {
     while (refs.length > 0) {
-        var _a = refs.pop(), node = _a[0], values = _a[1];
-        var target = void 0;
+        let [node, values] = refs.pop();
+        let target;
         if (node.hasAttribute("slot")) {
             target = node;
         }
@@ -128,8 +127,8 @@ function go(refs, modifiers) {
         }
         if (!target)
             continue;
-        var value = void 0;
-        var key = target.getAttribute("slot");
+        let value;
+        let key = target.getAttribute("slot");
         if (key) {
             value = values[key];
         }
@@ -140,40 +139,38 @@ function go(refs, modifiers) {
         if (!value)
             continue;
         if (Array.isArray(value)) {
-            var template = target.querySelector("template");
-            for (var _i = 0, value_1 = value; _i < value_1.length; _i++) {
-                var item = value_1[_i];
-                var clone = document.importNode(template.content, true);
-                for (var i = 0; i < clone.children.length; i++) {
+            let template = target.querySelector("template");
+            for (let item of value) {
+                let clone = document.importNode(template.content, true);
+                for (let i = 0; i < clone.children.length; i++) {
                     refs.push([clone.children[i], item]);
                 }
                 template.parentElement.insertBefore(clone, template);
             }
             template.remove();
             if (target.hasAttribute("onempty") && value.length == 0) {
-                var handler = newFunction(target.getAttribute("onempty"));
+                let handler = newFunction(target.getAttribute("onempty"));
                 target.removeAttribute("onempty");
                 handler.call(target, { target: target, value: "" });
             }
         }
         else {
             if (target.children.length > 0) {
-                for (var i = 0; i < target.children.length; i++) {
+                for (let i = 0; i < target.children.length; i++) {
                     refs.push([target.children[i], value]);
                 }
             }
             else {
-                var appliedModifiers = void 0;
+                let appliedModifiers;
                 if (target.hasAttribute("onmodify")) {
-                    var modifier = newFunction(target.getAttribute("onmodify"));
+                    let modifier = newFunction(target.getAttribute("onmodify"));
                     target.removeAttribute("onmodify");
                     appliedModifiers = [modifier].concat(modifiers);
                 }
                 else {
                     appliedModifiers = modifiers;
                 }
-                for (var _b = 0, appliedModifiers_1 = appliedModifiers; _b < appliedModifiers_1.length; _b++) {
-                    var modifier = appliedModifiers_1[_b];
+                for (let modifier of appliedModifiers) {
                     if (modifier.call(target, { target: target, value: value }) !== false)
                         break;
                 }
